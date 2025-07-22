04634026aad01e80d69e89dc0b1cce350e886317
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-07-22 17:34:12 +0100

    files added and renamed to set up for MVC structure

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Scene from "./scene";
-import Keypad from "./components/UI/keypad.jsx";
 
 function App() {
   return (
     <>
-      <Keypad />
       <Canvas style={{ height: "80vh", width: "100vw" }}>
         <Scene />
       </Canvas>
---
+++ view/src/classes/cubie.js
@@ -0,0 +1,6 @@
+function Cubie(id, type, colours, position) {
+  this.id = id;
+  this.type = type;
+  this.colours = colours;
+  this.position = position;
+}
