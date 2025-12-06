dab7cd336bc574c11c96b4a5769431f4ba376337
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-12-06 14:00:10 +0000

    removing corner from bottom if they are both in bottom layer and disconnected

--- model/models/solve/f2l/identifyCase.js
+++ model/models/solve/f2l/identifyCase.js
@@ -6,6 +6,7 @@ import {
 import returnEdgePosition from "../../helperFunctions/returnEdgePosition";
 import returnCornerPosition from "../../helperFunctions/returnCornerPosition";
 
+import moveCornerToTop from "./solveFunctions/setUps/moveCornerToTop";
 import cornerTopEdgeMid from "./solveFunctions/setUps/cornerTopEdgeMid";
 import cornerUpEdgeMid from "./solveFunctions/setUps/cornerUpEdgeMid";
 import cornerEdgeConnected from "./solveFunctions/setUps/cornerEdgeConnected";
@@ -16,22 +17,103 @@ import cornerBottomEdgeTop from "./solveFunctions/setUps/cornerBottomEdgeTop";
 const identifyCase = (cube, firstPair, secondPair) => {
   let tempCube = cube;
   const crossColour = getSolutionCrossColour();
-  const edge = returnEdgePosition(tempCube, firstPair, secondPair);
-  const corner = returnCornerPosition(tempCube, crossColour, [
+  let edge = returnEdgePosition(tempCube, firstPair, secondPair);
+  let corner = returnCornerPosition(tempCube, crossColour, [
     firstPair,
     secondPair
   ]);
-  const cornerFace = corner.face;
-  const cornerRow = corner.row;
-  const cornerCol = corner.col;
-  const edgeFace = edge.face;
-  const edgeRow = edge.row;
-  const edgeCol = edge.col;
+  let cornerFace = corner.face;
+  let cornerRow = corner.row;
+  let cornerCol = corner.col;
+  let edgeFace = edge.face;
+  let edgeRow = edge.row;
+  let edgeCol = edge.col;
   const sideFaces = ["front", "left", "back", "right"];
+  const bottomColToFace = {
+    0: "left",
+    2: "right"
+  };
+  const bottomRowToFace = {
+    0: "front",
+    2: "back"
+  };
+  const faceConversionClockwise = {
+    front: "left",
+    left: "back",
+    back: "right",
+    right: "front"
+  };
+  const faceConversionAnticlockwise = {
+    front: "right",
+    right: "back",
+    back: "left",
+    left: "front"
+  };
+  const edgeOnLeft = edgeCol === 0;
+  const cornerOnLeft = cornerCol === 0;
+  let cornerFaces = [];
 
-  // make case here if they are both not in top layer and arent even connected to get just the corner out seperately
+  let secondaryEdgeFace = "";
+
+  if (edgeOnLeft) {
+    secondaryEdgeFace = faceConversionClockwise[edgeFace];
+  } else {
+    secondaryEdgeFace = faceConversionAnticlockwise[edgeFace];
+  }
+
+  if (
+    cornerFace === "down" &&
+    edgeFace !== "top" &&
+    secondaryEdgeFace !== "top"
+  ) {
+    cornerFaces = [bottomColToFace[cornerCol], bottomRowToFace[cornerRow]];
+    if (
+      !cornerFaces.includes(edgeFace) ||
+      !cornerFaces.includes(secondaryEdgeFace)
+    ) {
+      // now seperate the corner
+      tempCube = moveCornerToTop(
+        tempCube,
+        cornerFace,
+        cornerFaces,
+        cornerOnLeft
+      );
+    }
+  } else if (
+    cornerRow === 2 &&
+    edgeFace !== "top" &&
+    secondaryEdgeFace !== "top"
+  ) {
+    cornerFaces = [
+      cornerFace,
+      cornerOnLeft
+        ? faceConversionClockwise[cornerFace]
+        : faceConversionAnticlockwise[cornerFace]
+    ];
+    if (
+      !cornerFaces.includes(edgeFace) ||
+      !cornerFaces.includes(secondaryEdgeFace)
+    ) {
+      tempCube = moveCornerToTop(
+        tempCube,
+        cornerFace,
+        cornerFaces,
+        cornerOnLeft
+      );
+    }
+  }
+  corner = returnCornerPosition(tempCube, crossColour, [firstPair, secondPair]);
+  edge = returnEdgePosition(tempCube, firstPair, secondPair);
+  cornerFace = corner.face;
+  cornerRow = corner.row;
+  cornerCol = corner.col;
+  edgeFace = edge.face;
+  edgeRow = edge.row;
+  edgeCol = edge.col;
 
   if (cornerFace !== "down" && cornerFace !== "top" && cornerRow === 0) {
+    // make case here if they are both not in top layer and arent even connected to get just the corner out seperately
+
     // is corner in top layer
     if (edgeFace === "top" || (edgeRow === 0 && sideFaces.includes(edgeFace))) {
       tempCube = cornerEdgeConnected(tempCube, corner, edge);
---
+++ model/models/solve/f2l/solveFunctions/setUps/moveCornerToTop.js
@@ -0,0 +1,52 @@
+import { B, D, F, L, R, U } from "../../../../moves/turns/index.js";
+import { x, y, z } from "../../../../moves/rotation/index.js";
+import { E } from "../../../../moves/slice/index.js";
+import {
+  getTempF2lSolution,
+  setTempF2lSolution
+} from "../../../../../store/solveStore.js";
+
+const moveCornerToTop = (cube, cornerFace, cornerFaces, cornerOnLeft) => {
+  console.log("Removing Corner");
+  let tempCube = cube;
+  const faceToMove = { front: F, left: L, back: B, right: R };
+  const faceToNotation = { front: "F", left: "L", back: "B", right: "R" };
+  const faceConversionClockwise = {
+    front: "left",
+    left: "back",
+    back: "right",
+    right: "front"
+  };
+  const faceConversionAnticlockwise = {
+    front: "right",
+    right: "back",
+    back: "left",
+    left: "front"
+  };
+
+  let turningFace = "";
+
+  if (cornerFace === "down") {
+    if (cornerFaces.includes("right")) {
+      turningFace = "right";
+    } else {
+      turningFace = "left";
+    }
+  } else {
+    turningFace = cornerFace;
+  }
+  setTempF2lSolution([
+    ...getTempF2lSolution(),
+    faceToNotation[turningFace] + (!cornerOnLeft ? "'" : ""),
+    "U",
+    faceToNotation[turningFace] + (cornerOnLeft ? "'" : "")
+  ]);
+
+  tempCube = faceToMove[turningFace](
+    !cornerOnLeft,
+    U(true, faceToMove[turningFace](cornerOnLeft, tempCube))
+  );
+  return tempCube;
+};
+
+export default moveCornerToTop;
